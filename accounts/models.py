9eee4d260f5e44a1a1d22a03793cfffed0ce863a
alchemist1111 <emoyoeugine@gmail.com>
2025-07-30 02:35:32 +0300

    Updated user models, configured base user manager and performed migration

--- accounts/models.py
+++ accounts/models.py
@@ -1,9 +1,27 @@
 from django.db import models
-from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import AbstractUser, BaseUserManager
+
+# Custom user manager
+class CustomUserManager(BaseUserManager):
+    def create_user(self, email, password=None, **extra_fields):
+        if not email:
+            raise ValueError('The Email field must be set')
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, email, password=None, **extra_fields):
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+
+        return self.create_user(email, password, **extra_fields)
 
 # User model inheriting from AbstractUser
 class User(AbstractUser):
     # Additional fields
+    email = models.EmailField(unique=True, db_index=True)
     phone_number = models.CharField(max_length=20, null=True, blank=True)
     date_of_birth = models.DateField(null=True, blank=True)
     
@@ -14,6 +32,11 @@ class User(AbstractUser):
     
     def __str__(self):
         return f"Name: {self.first_name} {self.last_name}"
+    
+    objects = CustomUserManager()
+    
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
     
 
 # UserProfile model to extend user information
