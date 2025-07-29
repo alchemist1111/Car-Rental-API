894390e781e907c4ba57d862d8b1eafd361edcf9
alchemist1111 <emoyoeugine@gmail.com>
2025-07-30 02:40:47 +0300

    Registered user and user profile

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from .models import User, UserProfile
 
-# Register your models here.
+# User admin registration
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    list_display = ('email', 'first_name', 'last_name', 'is_staff', 'is_active')
+    search_fields = ('email', 'first_name', 'last_name')
+    ordering = ('email',)
+    
+    
+# UserProfile admin registration
+@admin.register(UserProfile)
+class UserProfileAdmin(admin.ModelAdmin):
+    list_display = ('user', 'address')
+    search_fields = ('user__email', 'address')
+    ordering = ('user__email',)    
