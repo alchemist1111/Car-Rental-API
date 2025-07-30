7e5e8f859e56e03643458831a830f7e15084548a
alchemist1111 <emoyoeugine@gmail.com>
2025-07-30 17:55:41 +0300

    Registered vehicles, car types and car reviews in admin file

--- vehicles/admin.py
+++ vehicles/admin.py
@@ -1,3 +1,30 @@
 from django.contrib import admin
+from .models import Vehicle, CarType, CarReview
 
-# Register your models here.
+# Vehicle model registration
+@admin.register(Vehicle)
+class VehicleAdmin(admin.ModelAdmin):
+    list_display = ( 'car_type', 'make', 'model', 'year', 'registration_number', 'available', 'price_per_day')
+    search_fields = ('make', 'model', 'registration_number')
+    list_filter = ('available', 'year', 'car_type')
+    ordering = ('-year',)
+    
+    
+# CarType model registration
+@admin.register(CarType)
+class CarTypeAdmin(admin.ModelAdmin):
+    list_display = ('name', 'description')
+    search_fields = ('name',)
+    ordering = ('name',)
+    
+    
+# CarReview model registration
+@admin.register(CarReview)
+class CarReviewAdmin(admin.ModelAdmin):
+    list_display = ('vehicle', 'user', 'rating', 'comment')
+    search_fields = ('vehicle__make', 'comment')
+    list_filter = ('rating',)
+    ordering = ('-rating',)        
+
+
+
